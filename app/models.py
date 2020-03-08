08f48b6da23ef8606feb258e6ffae58bee5a84af
habib <h.hourany@gmail.com>
2020-03-08 14:24:32 +0100

    Add organisation, display connected user

--- app/models.py
+++ app/models.py
@@ -5,9 +5,13 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    first_name = db.Column(db.String(120), nullable=False)
+    last_name = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    
+    organisation_id = db.Column(db.Integer, db.ForeignKey('organisation.id'), nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,3 +22,13 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+class Organisation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    
+    users = db.relationship('User', backref='organisation', lazy='dynamic')
+    
+    def __repr__(self):
+        return '<Organisation {}>'.format(self.name)
+    
