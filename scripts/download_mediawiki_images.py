a6d4306334e19bcdd8c6614c68ceb9d6b24ffef0
habib <h.hourany@gmail.com>
2020-03-25 22:25:34 +0100

    add script to download images from wikimedia commons

---
+++ scripts/download_mediawiki_images.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import shutil
+import mwclient
+
+DIR = 'C:/Users/HabibElHourani/Desktop/tools/test2/longevity/data/images'
+
+site = mwclient.Site('commons.wikimedia.org')
+
+categories = [
+    'Caedmon manuscript',
+    'Bible_of_Wenceslaus_IV.'
+    ]
+
+for category_name in categories : 
+    category = site.Categories[category_name]
+    category_path = Path(DIR).joinpath(category_name)
+    if category_path.exists():
+        shutil.rmtree(category_path)
+        
+    category_path.mkdir()
+    
+    images = category.members(namespace=6)
+    for img in images:
+        file_name = img.page_title
+        file_path = category_path.joinpath(file_name)
+        file_path.write_bytes(img.download())
